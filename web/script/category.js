a2eb674990afe32512ae1f9232099cda63fc7693
Linh Nguyen <linh.nd@eastgate-software.com>
2023-12-30 10:10:48 +0700

    ok

--- web/script/category.js
+++ web/script/category.js
@@ -13,3 +13,40 @@ bar.addEventListener("click", function(event) {
     
     show = !show;
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var categoryElement = document.querySelector('.category-title');
+    var idCategory = 0;
+    if (categoryElement) {
+        idCategory = categoryElement.getAttribute('idCategory');
+    }
+
+    var price = document.getElementById('sort-price');
+    var seller = document.getElementById('sort-selling');
+
+    price.addEventListener('click', function() {
+        if (price.classList.contains('fa-arrow-up')) {
+            price.classList.remove('fa-arrow-up');
+            price.classList.add('fa-arrow-down');
+
+            
+        } else {
+            price.classList.remove('fa-arrow-down');
+            price.classList.add('fa-arrow-up');
+        }
+    });
+
+    seller.addEventListener('click', function() {
+        if (seller.classList.contains('fa-arrow-up')) {
+            seller.classList.remove('fa-arrow-up');
+            seller.classList.add('fa-arrow-down');
+        } else {
+            seller.classList.remove('fa-arrow-down');
+            seller.classList.add('fa-arrow-up');
+        }
+    });
+});
+
+function send(id, price, seller) {
+    window.location.href = "/shop/categories?id="+ id + "&price=" + price + "&seller=" + seller;
+}
